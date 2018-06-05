03febf651678743d776a66553a3e0515b2f14fa2
Ashok <coderatlabs@gmail.com>
2018-06-05 11:09:07 +0530

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  context: __dirname,
+  entry: './app/index.js',
+  devtool: 'cheap-eval-source-map',
+  output: {
+    path: path.join(__dirname, 'static'),
+    filename: 'build.js'
+  },
+  resolve: {
+    extensions: ['.js', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader'
+      }  
+    ]
+  }
+}
