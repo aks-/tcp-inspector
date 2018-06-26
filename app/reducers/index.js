deeb92a2ad2b3e9a230188899c8b4f5776a6c8aa
Ashok <coderatlabs@gmail.com>
2018-06-26 10:33:08 +0530

    fix bug

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,25 +1,25 @@
 const defaultState = {
   interfaces: [],
-  selectedInterface: "en0",
+  selectedInterface: null,
   parsers: []
 }
 
 const tcpInspectorApp = (state = defaultState, action) => {
   switch (action.type) {
     case 'select-interface':
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         selectedInterface: action.selectedInterface
       })
       break;
     case 'install-parser':
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         parsers: state.parsers.push(action.parser)
       })
     case 'remove-parser':
       const { parser } = action
       const indexOfParser = state.parsers.indexOf(parser)
       if (indexOfParser === -1) return state
-      return Object.assign(state, {
+      return Object.assign({}, state, {
         parsers: state.parsers.splice(indexOfParser, 1)
       })
     default:
